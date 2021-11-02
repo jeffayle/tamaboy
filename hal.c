4063accb756e45e123b08ae789b302b3fc83ea07
Jeffrey Aylesworth <jeffrey@aylesworth.ca>
2021-11-02 17:15:20 -0400

    turn lcd icons on and off

--- hal.c
+++ hal.c
@@ -2,6 +2,10 @@
 #include "tamalib/tamalib.h"
 #include "hal_types.h"
 #include "hal.h"
+#include "rom.h"
+
+/* defined in kernel.c */
+void copy_mono_pixels(int* dest, int graphics, char zero, char one);
 
 static void do_nothing(void) {
     return;
@@ -20,8 +24,17 @@ static void set_lcd_matrix(u8_t x, u8_t y, bool_t val) {
     index = y*32 + x;
     LCD_BUFFER[index] = val;
 }
-static void set_lcd_icon(u8_t icon, bool_t val) {
-
+static void set_lcd_icon(u8_t icon, bool_t en) {
+    int x = (icon&3)*4;
+    int y = (icon>=4)?4:0;
+    int col = en ? 1 : 3;
+    for (int dx=0; dx<4; dx++) {
+        for (int dy=0; dy<4; dy++) {
+           int i = 2 * ( (y+dy)*16 + dx+x );
+           copy_mono_pixels(TILES+8*(i+4), LCD_ICONS_RAW[i], 2,col);
+           copy_mono_pixels(TILES+8*(i+5), LCD_ICONS_RAW[i+1], 2,col);
+        }
+    }
 }
 
 static void set_frequency(u32_t freq) {
